2a9c7804be6ce2878405e94edf470fd1c06e8fa4
Paul Chicarello <paul@chicarello.com>
2017-06-18 21:09:44 -0400

    This is a somewhat working version of this cookbook

--- metadata.rb
+++ metadata.rb
@@ -8,3 +8,8 @@ version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 supports 'centos'
+
+depends 'mysql', '~> 8.4.0'
+depends 'php', '~> 4.2.0'
+depends 'yum-mysql-community', '~> 2.1.0'
+depends 'selinux', '~> 2.0.3'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,29 @@
 # Cookbook:: pjc_lamp
 # Recipe:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# Copyright (C) 2017 Paul chicarello
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe 'php::default'
+include_recipe 'php::module_mysql'
+include_recipe 'yum-mysql-community::mysql55'
+include_recipe 'selinux::permissive'
+
+mysql_service 'lamp' do
+  port '3306'
+  version '5.5'
+  initial_root_password 'Ch4ng3me'
+  action [:create, :start]
+end
